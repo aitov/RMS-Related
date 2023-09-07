7063f8ae88fda94454cb58da7b9b602c1e329294
alexaitov <aaitov@gmail.com>
2023-09-07 20:59:48 +0300

    add support of local properties

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -1,16 +1,19 @@
-# Raspberry Pi
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  source ~/vRMS/bin/activate
-  rms_folder=~/source/RMS
-  home_folder=/home
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  # Mac Apple Silicon
-  if [[ $(uname -m) == 'arm64' ]]; then
-    source ~/.conda/envs/rms/bin/activate
-  # Mac Intel
-  elif [[ $(uname -m) == 'x86_64' ]]; then
-    source ~/anaconda3/envs/rms/bin/activate
-  fi
-  rms_folder=~/home/projects/RMS
-  home_folder=~/home
+# Inits variables and activates python env
+# For execution on RMS Raspberry Pi - no need do additional action - used default values
+# For local execution : required variables could be overriden required by local paths
+# Use local.properties.template file as example, override required properties and copy it with name : local.properties
+
+# Default properties for Raspberry Pi
+venv=~/vRMS
+rms_folder=~/source/RMS
+home_folder=/home
+
+if [ -e "local.properties" ]; then
+  source "local.properties"
 fi
+
+echo "venv: $venv"
+echo "rms_folder: $rms_folder"
+echo "home_folder: $home_folder"
+
+source "$venv/bin/activate"
