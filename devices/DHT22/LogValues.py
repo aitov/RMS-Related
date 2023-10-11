711439796d13e38b3a174f632243884f91384c4b
alexaitov <aaitov@gmail.com>
2023-10-11 10:37:31 +0300

    add cpu temp to logs

--- devices/DHT22/LogValues.py
+++ devices/DHT22/LogValues.py
@@ -2,15 +2,15 @@
 #  Log values of DHT22 sensor to csv file for statistic collection
 #
 
-import adafruit_dht, time, os
+import adafruit_dht, time, os, subprocess
 # Need specify pin where DHT22 connected
 from board import D10
 
 dht_device = adafruit_dht.DHT22(D10, use_pulseio=False)
 
 # check temperature and save once in 5 minutes
-timeout = 60 * 5
-
+# timeout = 60 * 5
+timeout = 5
 csvFolder = "/home/pi/log"
 #  RMS should restart Raspberry every day, it should force create new file in next month
 csvFile = time.strftime("%m.%Y") + "_outside_sensor_values.csv"
@@ -22,7 +22,7 @@ csvFilePath = os.path.join(csvFolder, csvFile)
 
 if not os.path.isfile(csvFilePath):
     file = open(csvFilePath, "w")
-    file.write('Date, Temperature, Humidity\n')
+    file.write('Date, Temperature, CPU Temperature, Humidity\n')
 else:
     file = open(csvFilePath, "a")
 
@@ -34,10 +34,14 @@ while running:
         # read the humidity and temperature
         temperature = dht_device.temperature
         humidity = dht_device.humidity
+        # get CPU temp
+        cpu_temperature = subprocess.check_output(["/opt/vc/bin/vcgencmd measure_temp | cut -c6-9"], shell=True, encoding="utf-8").strip()
 
         if temperature is not None and humidity is not None:
+            print(cpu_temperature)
             file.write(
                 time.strftime("%d.%m.%Y %H:%M:%S") + ", " + "{0:0.1f}".format(temperature) + ", "
+                + cpu_temperature  + ", "
                 + "{0:0.1f}".format(humidity) + '\n')
             file.flush()
             time.sleep(timeout)
@@ -49,3 +53,4 @@ while running:
         print('Program stopped')
         running = False
         file.close()
+
