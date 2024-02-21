f331c02ec789deb7e181e106e27677ef497255af
alexaitov <aaitov@gmail.com>
2024-02-21 21:22:03 +0200

    added shower name to image

--- Utils/RmsProcessing.py
+++ Utils/RmsProcessing.py
@@ -1,6 +1,9 @@
 from RMS.Formats.FTPdetectinfo import readFTPdetectinfo, findFTPdetectinfoFile
 import RMS.Formats.FFfile as file
 from RMS.Routines.CustomPyqtgraphClasses import *
+from RMS.Formats import FFfile
+from Utils.ShowerAssociation import showerAssociation
+import RMS.ConfigReader as cr
 import pyqtgraph as pg
 from PyQt5.QtCore import *
 import os, datetime
@@ -54,7 +57,7 @@ class RmsProcessing(QtWidgets.QMainWindow):
 
 
     def loadImage(self, imageName):
-        folder_path = "/Users/alexaitov/home/pi/RMS_data/ArchivedFiles/UA0003_20231023_152622_865748"
+        folder_path = "/Users/alexaitov/home/pi/RMS_data/ArchivedFiles/UA0004_20240108_145148_433498"
         # folder_path = "/Users/alexaitov/home/pi/RMS_data/ArchivedFiles/UA0004_20231109_145806_373961"
 
         return np.swapaxes(file.read(folder_path, imageName).maxpixel, 0, 1)
@@ -62,43 +65,53 @@ class RmsProcessing(QtWidgets.QMainWindow):
     def addImage(self, meteorData, layout):
         horizontalLayout = QtWidgets.QHBoxLayout()
         horizontalLayout.addStretch()
-        horizontalLayout.addLayout(self.createCheckBoxesView(meteorData, True, True))
+        horizontalLayout.addLayout(self.createCheckBoxesView(meteorData, True))
         viewWidget = pg.GraphicsView()
         viewWidget.setFixedHeight(720)
         viewWidget.setFixedWidth(1280)
         # create image view
-        viewWidget.setCentralWidget(self.createImageView(meteorData.fileName))
+        viewWidget.setCentralWidget(self.createImageView(meteorData))
         horizontalLayout.addWidget(viewWidget)
         horizontalLayout.addStretch()
         horizontalLayout.setAlignment(Qt.AlignCenter)
 
         layout.addLayout(horizontalLayout)
 
-    def addTimestampToImage(self, imgFrame, imageName):
+    def addTimestampToImage(self, imgFrame, meteorData):
         label = pg.TextItem(color=(255, 255, 255))
         label.setFont(QtGui.QFont('monospace', 8))
         label.setTextWidth(200)
         label.setZValue(1000)
-        label.setText(self.getTimestampTitle(imageName))
+        label.setText("{:d} - {:s}".format(int(meteorData.meteorNumber), self.getTimestampTitle(meteorData.fileName)))
         label.setParentItem(imgFrame)
         label.setPos(0, 720 - label.boundingRect().height())
 
-    def createImageView(self, imageName):
+    def addShowerNameToImage(self, imgFrame, showerName):
+        label = pg.TextItem(color=(255, 255, 255))
+        label.setFont(QtGui.QFont('monospace', 8))
+        label.setTextWidth(200)
+        label.setZValue(1000)
+        label.setText("Meteor shower : {:s}".format(showerName))
+        label.setParentItem(imgFrame)
+        label.setPos(165, 720 - label.boundingRect().height())
+
+    def createImageView(self, meteorData):
         imageView = ViewBox()
         imageView.invertY()
         imageView.setMouseEnabled(False, False)
-        img = pg.ImageItem(self.loadImage(imageName))
+        img = pg.ImageItem(self.loadImage(meteorData.fileName))
         imageView.addItem(img, False)
         imageView.autoRange(padding=0)
         # add timestamp to image
-        self.addTimestampToImage(imageView, imageName)
+        self.addTimestampToImage(imageView, meteorData)
+        self.addShowerNameToImage(imageView, meteorData.showerName)
         return imageView
 
-    def createCheckBoxesView(self, meteorData, detected, addToStack):
+    def createCheckBoxesView(self, meteorData, addToStack):
         verticalLayout = QtWidgets.QVBoxLayout()
         verticalLayout.addStretch()
         detectedCheckBox = QtWidgets.QCheckBox("Detected")
-        detectedCheckBox.setChecked(detected)
+        detectedCheckBox.setChecked(meteorData.showerName != "Unknown")
         detectedCheckBox.meteorId = meteorData.fileName + "_" + str(meteorData.meteorNumber)
         self.detectedCheckBoxes[detectedCheckBox.meteorId] = detectedCheckBox
         detectedCheckBox.toggled.connect(self.onDetectedClick)
@@ -112,12 +125,11 @@ class RmsProcessing(QtWidgets.QMainWindow):
         return verticalLayout
 
 
-    def getTimestampTitle(self, fr_path):
-        _, fname = os.path.split(fr_path)
-        splits = fname.split('_')
-        dtstr = splits[2] + '_' + splits[3] + '.' + splits[4]
-        imgdt = datetime.datetime.strptime(dtstr, '%Y%m%d_%H%M%S.%f')
-        return splits[1] + ' ' + imgdt.strftime('%Y-%m-%d %H:%M:%S UTC')
+    def getTimestampTitle(self, ff_path):
+        fileName = os.path.basename(ff_path)
+        stationName = fileName.split('_')[1]
+        timestampt = FFfile.filenameToDatetime(fileName)
+        return stationName + ' ' + timestampt.strftime('%Y-%m-%d %H:%M:%S UTC')
 
     def onDetectedClick(self):
         detectedCheckBox = self.sender()
@@ -136,22 +148,43 @@ class RmsProcessing(QtWidgets.QMainWindow):
         self.statusBar.showMessage("Detected meteors: {}/{}".format(self.detectedCount, self.detectedTotal))
 
 class MeteorData(object):
-    def __init__(self, fileName=None, meteorNumber=None):
+    def __init__(self, fileName=None, meteorNumber=None, showerName=None):
         self.fileName = fileName
         self.meteorNumber = meteorNumber
+        self.showerName = showerName
 
     def readDetectedMeteors(self):
-        folder_path = "/Users/alexaitov/home/pi/RMS_data/ArchivedFiles/UA0003_20231023_152622_865748"
+        folder_path = "/Users/alexaitov/home/pi/RMS_data/ArchivedFiles/UA0004_20240108_145148_433498"
         # folder_path = "/Users/alexaitov/home/pi/RMS_data/ArchivedFiles/UA0004_20231109_145806_373961"
+        config = cr.loadConfigFromDirectory([folder_path + "/.config"], 'notused')
         ftp_detect_file = findFTPdetectinfoFile(folder_path)
         meteor_list = readFTPdetectinfo(folder_path, ftp_detect_file)
-        return self.getDetectedMeteors(meteor_list)
+        associations_per_dir, _ = showerAssociation(config, [ftp_detect_file],
+                                                    shower_code=None, show_plot=False, save_plot=False, plot_activity=False)
+        associations = {}
+        associations.update(associations_per_dir)
+        return self.getDetectedMeteors(meteor_list, associations)
 
-    def getDetectedMeteors(self, meteor_list):
+    def getDetectedMeteors(self, meteor_list, associations):
         meteors = []
         for meteor in meteor_list:
-            meteors.append(MeteorData(meteor[0], meteor[2]))
+            showerName = self.getMeteorShowerTitle(meteor[0], meteor[2], associations)
+            meteors.append(MeteorData(meteor[0], meteor[2], showerName))
         return meteors
+
+    def getMeteorShowerTitle(self, ffFile, meteorNumber, associations):
+        title = "Unknown"
+        # get all available meteors for current FF file
+
+        # search first suitable by time range
+        if (ffFile, meteorNumber) in associations:
+            shower = associations[(ffFile, meteorNumber)][1]
+            if shower is not None:
+                title = "[{:s}] - {:s}".format(shower.name, shower.name_full)
+            else:
+                title = "Sporadic"
+
+        return title
 
 
 # try:
