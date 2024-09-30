0d0b2e8a36f21044848b0827f9c0d7823ec92cd4
alexaitov <aaitov@gmail.com>
2024-09-30 23:06:43 +0300

    RPi version

--- scripts/init_rms_related.sh
+++ scripts/init_rms_related.sh
@@ -19,25 +19,26 @@ if [ ! -d "RMS-Related" ]; then
   echo "Checkout RMS-Related"
   git clone https://github.com/aitov/RMS-Related.git
   cd "RMS-Related"
+  # TODO remove
+  git checkout dev
+  find . -type f -name "*.sh" -print0 |
+    while IFS= read -r -d '' sh_file; do
+      if [[ ! -x "$sh_file" ]]; then
+        echo "Make executable : $sh_file"
+        chmod +x "$sh_file"
+      fi
+    done
+
+  cd "scripts"
+  echo "Create desktop links"
+  . create_desktop_links.sh
+  read -n 1 -s -r -p "Press any key to exit"
+  echo
 else
+  echo "Update RMS-Related"
   cd "RMS-Related"
   git reset --hard HEAD
   git pull
 fi
 
-find . -type f -name "*.sh" -print0 |
-  while IFS= read -r -d '' sh_file; do
-    if [[ ! -x "$sh_file" ]]; then
-      echo "Make executable : $sh_file"
-      chmod +x "$sh_file"
-    fi
-  done
-
-cd "scripts"
-
-. create_desktop_links.sh
-
-read -n 1 -s -r -p "Press any key to exit"
-echo
-exit
 
