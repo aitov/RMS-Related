2787c16f945fecf93012e629ba8122cdc78e46ef
alexaitov <aaitov@gmail.com>
2025-07-02 20:53:26 +0300

    added create full archive file

---
+++ scripts/CreateFullArchive.py
@@ -0,0 +1,16 @@
+from copy import deepcopy
+from RMS.Formats.FTPdetectinfo import readFTPdetectinfo, findFTPdetectinfoFile
+from RMS.ArchiveDetections import archiveDetections
+
+def createFullArchive(captured_night_dir, archived_night_dir, config):
+    print("Executing creating full archive")
+    print("Captured dir path : {}".format(captured_night_dir))
+    print("Archived dir path : {}".format(archived_night_dir))
+
+    ftp_detect_file = findFTPdetectinfoFile(captured_night_dir)
+    ff_detected = readFTPdetectinfo(captured_night_dir, ftp_detect_file)
+    full_archive_dir = archived_night_dir + "full"
+    newConfig = deepcopy(config)
+    newConfig.upload_mode = 1
+    archive_name = archiveDetections(captured_night_dir, full_archive_dir, ff_detected, newConfig)
+    print("Archived to  : {}".format(archive_name))
