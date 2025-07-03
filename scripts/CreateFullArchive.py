b0a73a2b06081acefdb7e2c6fc7d511de5868c04
alexaitov <aaitov@gmail.com>
2025-07-03 09:14:58 +0300

    added create full archive file

--- scripts/CreateFullArchive.py
+++ scripts/CreateFullArchive.py
@@ -1,11 +1,17 @@
 from copy import deepcopy
 from RMS.Formats.FTPdetectinfo import readFTPdetectinfo, findFTPdetectinfoFile
 from RMS.ArchiveDetections import archiveDetections
+from RMS.ConfigReader import loadConfigFromDirectory
 
 def createFullArchive(captured_night_dir, archived_night_dir, config):
     print("Executing creating full archive")
     print("Captured dir path : {}".format(captured_night_dir))
     print("Archived dir path : {}".format(archived_night_dir))
+
+    if config is None:
+        print("No config file provided")
+        config = loadConfigFromDirectory(None, 'notused')
+        print("Loaded default config")
 
     ftp_detect_file = findFTPdetectinfoFile(captured_night_dir)
     ff_detected = readFTPdetectinfo(captured_night_dir, ftp_detect_file)
