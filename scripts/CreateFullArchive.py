503eef8edbac0c5394abe81f8d9dd32fccc2968f
alexaitov <aaitov@gmail.com>
2025-08-02 14:44:52 +0300

    fix file name

--- scripts/CreateFullArchive.py
+++ scripts/CreateFullArchive.py
@@ -63,10 +63,10 @@ def getExtraFiles(captured_path):
         if ((file_name.lower().endswith('.kml'))
                 or (file_name.lower().endswith('.json'))
                 or (file_name.lower().endswith('.ecsv'))
-                or (file_name.lower().endswith('.json'))
                 or (file_name.lower().endswith('.txt'))
                 or (file_name.lower().endswith('.csv'))
                 or (file_name.lower().endswith('.cal'))
+                or (file_name.lower().startswith('flux_') and file_name.lower().endswith('.png'))
                 or (file_name.lower().endswith('_timelapse.mp4'))):
             extra_files.append(os.path.join(captured_path, file_name))
     return extra_files
