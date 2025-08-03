a80b375c9cf059989bd935cc6bf3fca781bef860
alexaitov <aaitov@gmail.com>
2025-08-03 13:19:08 +0300

    fix file name

--- scripts/create_full_archive.sh
+++ scripts/create_full_archive.sh
@@ -21,7 +21,7 @@ source_folder_name=$(basename "$source_folder")
 results_folder="$archived_files/$source_folder_name"
 
 cd "$rms_folder"
-python ""$home_folder/RMS-Related/scripts/CreateFullArchive.py" "$source_folder" "$results_folder" "False"
+python "$home_folder/RMS-Related/scripts/CreateFullArchive.py" "$source_folder" "$results_folder" "False"
 
 read -n 1 -s -r -p "Press any key to exit"
 echo
